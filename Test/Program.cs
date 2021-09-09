d0aed2ea83a3fae8c1fa24ac471eebfc36f2f1eb
AaronSong321 <2511146542@qq.com>
2021-09-09 18:49:34 +0100

    version 1.5.2

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,12 +4,6 @@ using System.IO.Compression;
 using System.Linq;
 using MiranaCompiler;
 
-const string src = "../../../../MiranaCompiler/bin/Release/net5.0";
-string dst = Path.GetFullPath($"{src}/Mirana-{Compiler.Version}.zip");
-if (Directory.Exists(src)) {
-    File.Delete(dst);
-    ZipFile.CreateFromDirectory(src, dst);
-}
 
 var compiler = new Compiler();
 await compiler.CompileAsync(new [] {
